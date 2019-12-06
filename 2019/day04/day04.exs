ae9fafe8ba303f82bf94a1055cf3f504b878d21f
Steve Hill <stevhil+github@gmail.com>
2019-12-06 20:00:43 +0000

    Tidy up Day04

--- 2019/day04/day04.exs
+++ 2019/day04/day04.exs
@@ -8,16 +8,18 @@ defmodule Day04 do
   def count_groups(num) do
     Integer.to_string(num)
     |> String.graphemes()
-    |> Enum.chunk_by(&(&1))
+    |> Enum.chunk_by(& &1)
     |> Enum.map(&Enum.count/1)
   end
 
   def valid_p1?(num) do
-    not has_descender?(Integer.digits(num)) and Enum.any?(count_groups(num), &(&1 >= 2))
+    not has_descender?(Integer.digits(num)) and
+      Enum.any?(count_groups(num), &(&1 >= 2))
   end
 
   def valid_p2?(num) do
-    not has_descender?(Integer.digits(num)) and Enum.any?(count_groups(num), &(&1 == 2))
+    not has_descender?(Integer.digits(num)) and
+      Enum.any?(count_groups(num), &(&1 == 2))
   end
 
   def check(from, to, valid_fun) do
@@ -25,7 +27,6 @@ defmodule Day04 do
     |> Enum.filter(valid_fun)
     |> Enum.count()
   end
-
 end
 
 IO.puts("Part 1: #{Day04.check(245_182, 790_572, &Day04.valid_p1?/1)}")
